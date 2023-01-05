1b49cd2083ed03ba22aef64a2700e27cc7162e2a
eckonuke <116886281+eckonuke@users.noreply.github.com>
2023-01-05 17:17:42 +0900

    .

--- Source/Sifu/Sifu.Build.cs
+++ Source/Sifu/Sifu.Build.cs
@@ -8,7 +8,7 @@ public class Sifu : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","UMG","NavigationSystem","AIMode" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","UMG","NavigationSystem","AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
