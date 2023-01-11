0daec27f87c500e56109e6a6cbd4d0a6ea0eb721
eckonuke <116886281+eckonuke@users.noreply.github.com>
2023-01-11 17:58:42 +0900

    .

--- Source/Sifu/Private/PlayerTest.cpp
+++ Source/Sifu/Private/PlayerTest.cpp
@@ -3,26 +3,138 @@
 
 #include "PlayerTest.h"
 
+#include <GameFramework/SpringArmComponent.h>
+#include <GameFramework/Character.h>
+#include <Camera/CameraComponent.h>
+#include <Components/SphereComponent.h>
+#include "HJ_Enemy.h"
+#include "EnemyFSM.h"
+#include <Engine/SkeletalMesh.h>
+#include <Kismet/GameplayStatics.h>
+#include <Components/CapsuleComponent.h>
+
+#include <Components/SphereComponent.h>
+#include "HJ_Enemy.h"
+#include "EnemyFSM.h"
+#include <Engine/SkeletalMesh.h>
+#include <GameFramework/Character.h>
+#include <Kismet/GameplayStatics.h>
+#include <Components/CapsuleComponent.h>
+#include <Animation/AnimMontage.h>
+#include <GameFramework/CharacterMovementComponent.h>
+#include "PlayerAnim.h"
+
 // Sets default values
 APlayerTest::APlayerTest()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempMesh(TEXT("SkeletalMesh'/Game/Mannequin/Character/Mesh/UE4_Mannequin.UE4_Mannequin'"));
+	if (tempMesh.Succeeded()) {
+		GetMesh()->SetSkeletalMesh(tempMesh.Object);
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -90), FRotator(0, -90, 0));
+	}
+
+	//springArm 컴포넌트 붙이기
+	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	springArmComp->SetupAttachment(RootComponent);
+	springArmComp->SetRelativeLocation(FVector(0, 70, 90));
+	springArmComp->TargetArmLength = 400;
+	springArmComp->bUsePawnControlRotation = true;
+
+	//Camera 컴포넌트 붙이기
+	camComp = CreateDefaultSubobject<UCameraComponent>(TEXT("camComp"));
+	camComp->SetupAttachment(springArmComp);
+	camComp->bUsePawnControlRotation = false;
+	bUseControllerRotationYaw = true;
+
+	//애니메이션 블루프린트 가져오자
+	ConstructorHelpers::FClassFinder<UPlayerAnim> tempAnim(TEXT("AnimBlueprint'/Game/Mannequin/Animations/ThirdPerson_AnimBP.ThirdPerson_AnimBP_C'"));
+	if (tempAnim.Succeeded()) {
+		GetMesh()->SetAnimInstanceClass(tempAnim.Class);
+	}
+
+	//피격 애니메이션
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempStomach(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Big_Stomach_Hit_Montage.Big_Stomach_Hit_Montage'"));
+	if (tempStomach.Succeeded()) {
+		stomach = tempStomach.Object;
+	}
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempHead2(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Head_Hit_2_Montage.Head_Hit_2_Montage'"));
+	if (tempHead2.Succeeded()) {
+		head2 = tempHead2.Object;
+	}
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempHead3(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Head_Hit_3_Montage.Head_Hit_3_Montage'"));
+	if (tempHead3.Succeeded()) {
+		head3 = tempHead3.Object;
+	}
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempHead4(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Head_Hit_4_Montage.Head_Hit_4_Montage'"));
+	if (tempHead4.Succeeded()) {
+		head4 = tempHead4.Object;
+	}
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempFallDown(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Sweep_Fall_Montage.Sweep_Fall_Montage'"));
+	if (tempFallDown.Succeeded()) {
+		falldown = tempFallDown.Object;
+	}
+	//죽음
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempDeath(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Death.Death'"));
+	if (tempDeath.Succeeded()) {
+		death = tempDeath.Object;
+	}
+	//방어
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempBlock(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Block_Hit_Montage.Block_Hit_Montage'"));
+	if (tempBlock.Succeeded()) {
+		block = tempBlock.Object;
+	}
+	//주먹 공격 애니메이션
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempPunch(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/regular-punch.regular-punch'"));
+	if (tempPunch.Succeeded()) {
+		punch = tempPunch.Object;
+	}
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempJab(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Lead_Jab_Montage.Lead_Jab_Montage'"));
+	if (tempJab.Succeeded()) {
+		jab = tempJab.Object;
+	}
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempUpper(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Uppercut_Montage.Uppercut_Montage'"));
+	if (tempUpper.Succeeded()) {
+		uppercut = tempUpper.Object;
+	}
+	//발차기 공격 애니메이션
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempKick(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/kick-regular_Montage.kick-regular_Montage'"));
+	if (tempKick.Succeeded()) {
+		kick = tempKick.Object;
+	}
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempHighKick(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/kick-up_Montage.kick-up_Montage'"));
+	if (tempHighKick.Succeeded()) {
+		highKick = tempHighKick.Object;
+	}
+	ConstructorHelpers::FObjectFinder<UAnimMontage> tempLowKick(TEXT("AnimMontage'/Game/Mannequin/Animations/h2H_Anim/Leg_Sweep_Montage.Leg_Sweep_Montage'"));
+	if (tempLowKick.Succeeded()) {
+		lowKick = tempLowKick.Object;
+	}
 }
 
 // Called when the game starts or when spawned
-void APlayerTest::BeginPlay()
-{
+void APlayerTest::BeginPlay() {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
-void APlayerTest::Tick(float DeltaTime)
-{
+void APlayerTest::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
+	if (movementEnabled) {
+		direction = FTransform(GetControlRotation()).TransformVector(direction);
+		AddMovementInput(direction);
+		direction = FVector::ZeroVector;
+	}
+	setTarget();
+}
+
+void APlayerTest::NotifyActorBeginOverlap(AActor* OtherActor) {
+	Super::NotifyActorBeginOverlap(OtherActor);
 }
 
 // Called to bind functionality to input
@@ -30,5 +142,240 @@ void APlayerTest::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("Turn Right / Left Mouse"), this, &APlayerTest::Turn);
+	PlayerInputComponent->BindAxis(TEXT("Look Up / Down Mouse"), this, &APlayerTest::LookUp);
+	PlayerInputComponent->BindAxis(TEXT("Move Right / Left"), this, &APlayerTest::InputHorizontal);
+	PlayerInputComponent->BindAxis(TEXT("Move Forward / Backward"), this, &APlayerTest::InputVertical);
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &APlayerTest::InputJump);
+	PlayerInputComponent->BindAction(TEXT("Punch"), IE_Pressed, this, &APlayerTest::AttackPunch);
+	PlayerInputComponent->BindAction(TEXT("Kick"), IE_Pressed, this, &APlayerTest::AttackKick);
+	DECLARE_DELEGATE_OneParam(FBlock, bool);
+	PlayerInputComponent->BindAction<FBlock>(TEXT("Block"), IE_Pressed, this, &APlayerTest::PlayerBlock, true);
+	PlayerInputComponent->BindAction<FBlock>(TEXT("Block"), IE_Released, this, &APlayerTest::PlayerBlock, false);
+	DECLARE_DELEGATE_OneParam(FRun, bool);
+	PlayerInputComponent->BindAction<FRun>(TEXT("Run"), IE_Pressed, this, &APlayerTest::InputRun, true);
+	PlayerInputComponent->BindAction<FRun>(TEXT("Run"), IE_Released, this, &APlayerTest::InputRun, false);
+}
+
+void APlayerTest::Turn(float value) {
+	AddControllerYawInput(value);
+}
+void APlayerTest::LookUp(float value) {
+	AddControllerPitchInput(value);
+}
+void APlayerTest::InputHorizontal(float value) {
+	direction.Y = value;
+}
+void APlayerTest::InputVertical(float value) {
+	direction.X = value;
+}
+void APlayerTest::InputJump() {
+	Jump();
+}
+void APlayerTest::InputRun(bool run) {
+	if (run)
+		GetCharacterMovement()->MaxWalkSpeed = 900;
+	else
+		GetCharacterMovement()->MaxWalkSpeed = 600;
+}
+void APlayerTest::setTarget() {
+	if (!targetLock) {
+		FVector startPos = camComp->GetComponentLocation();
+		FVector endPos = startPos + camComp->GetForwardVector() * 5000;
+		FHitResult hitinfo;
+		FCollisionQueryParams params;
+		params.AddIgnoredActor(this);
+		bool bHit = GetWorld()->LineTraceSingleByChannel(hitinfo, startPos, endPos, ECC_Pawn, params);
+		if (bHit) {
+			AHJ_Enemy* enem = Cast<AHJ_Enemy>(hitinfo.GetActor());
+			if (enem) {
+				targetEnemy = enem;
+				targetLock = true;
+				UE_LOG(LogTemp, Warning, TEXT("%s"), *targetEnemy->GetName());
+			}
+		}
+	}
+}
+
+//공격 방어
+void APlayerTest::PlayerBlock(bool value) {
+	if (!isDead)
+		isBlocking = value;
+}
+
+//플레이어가 공격을 받았다
+void APlayerTest::OnHitDamage() {
+	if (!isDead) {
+		if (!isBlocking) {
+			//체력 감소
+			hp--;
+			//만약에 체력이 없다면
+			if (hp <= 0) {
+				isDead = true;
+				//상태를 죽음으로 전환
+				PlayerDie();
+				//캡슐 충돌체 비활성화
+				GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+				//GetComponentByClass(UCapsuleComponent::StaticClass())
+			}
+		}
+	}
+}
+
+//플레이어가 적을 타격
+void APlayerTest::PlayerDamage() {
+	if (targetLock) {
+		//거리 계산 (Player - enemy)
+		FVector v = GetActorLocation() - targetEnemy->GetActorLocation();
+		if (v.Length() <= 300) {
+			targetEnemy->fsm->OnDamageProcess();
+		}
+	}
+	//TArray<AActor*> enemys;
+	//UGameplayStatics::GetAllActorsOfClass(GetWorld(), AHJ_Enemy::StaticClass(), enemys);
+	//for (int i = 0; i < enemys.Num(); i++)
+	//{
+	//	//거리 계산 (Player - enemy)
+	//	FVector v = GetActorLocation() - enemys[i]->GetActorLocation();
+	//	float dist = v.Length();
+	//	if (dist < 300) {
+	//		AHJ_Enemy* e = Cast<AHJ_Enemy>(enemys[i]);
+	//		e->fsm->OnDamageProcess();
+	//	}
+	//}
+}
+
+//플레이어 죽음
+void APlayerTest::PlayerDie()
+{
+	PlayAnimMontage(death);
+}
+
+void APlayerTest::AttackPunch() {
+	movementEnabled = false;
+	kickorPunch = true;
+	if (IsAttacking) {
+		saveAttack = true;
+	}
+	else {
+		IsAttacking = true;
+		punchCombo();
+	}
+}
+
+void APlayerTest::AttackKick() {
+	movementEnabled = false;
+	kickorPunch = false;
+	if (IsAttacking) {
+		saveAttack = true;
+	}
+	else {
+		IsAttacking = true;
+		kickCombo();
+	}
+}
+
+//플레이어 공격 콤보
+void APlayerTest::saveAttackCombo() {
+	if (saveAttack) {
+		saveAttack = false;
+		if (kickorPunch) {
+			punchCombo();
+		}
+		else {
+			kickCombo();
+		}
+	}
+}
+
+//플레이어 콤보 애니메이션 스위치
+void APlayerTest::punchCombo() {
+	switch (punchCount)
+	{
+	case 0:
+		punchCount = 1;
+		//PlayerDamage();
+		PlayAnimMontage(punch);
+		break;
+	case 1:
+		punchCount = 2;
+		//PlayerDamage();
+		PlayAnimMontage(jab);
+		break;
+	case 2:
+		punchCount = 0;
+		//PlayerDamage();
+		PlayAnimMontage(uppercut);
+		break;
+	}
+}
+void APlayerTest::kickCombo() {
+	switch (kickCount)
+	{
+	case 0:
+		kickCount = 1;
+		//PlayerDamage();
+		PlayAnimMontage(kick);
+		break;
+	case 1:
+		kickCount = 2;
+		//PlayerDamage();
+		PlayAnimMontage(highKick);
+		break;
+	case 2:
+		kickCount = 0;
+		//PlayerDamage();
+		PlayAnimMontage(lowKick);
+		break;
+	}
+}
+//플레이어 공격콤보 재설정
+void APlayerTest::ResetCombo() {
+	IsAttacking = false;
+	punchCount = 0;
+	saveAttack = false;
+	kickCount = 0;
+	movementEnabled = true;
+}
+
+//Stomach hit
+void APlayerTest::HurtAnim0() {
+	if (!isDead) {
+		PlayAnimMontage(stomach);
+		OnHitDamage();
+		ResetCombo();
+	}
+}
+//head hit2
+void APlayerTest::HurtAnim1() {
+	if (!isDead) {
+		PlayAnimMontage(head2);
+		OnHitDamage();
+		ResetCombo();
+	}
+}
+//head hit3
+void APlayerTest::HurtAnim2() {
+	if (!isDead) {
+		PlayAnimMontage(head3);
+		OnHitDamage();
+		ResetCombo();
+	}
+}
+//head hit4
+void APlayerTest::HurtAnim3() {
+	if (!isDead) {
+		PlayAnimMontage(head4);
+		OnHitDamage();
+		ResetCombo();
+	}
+}
+//fall down 
+void APlayerTest::HurtAnim4() {
+	if (!isDead) {
+		PlayAnimMontage(falldown);
+		OnHitDamage();
+		ResetCombo();
+	}
 }
 
--- Source/Sifu/Public/PlayerTest.h
+++ Source/Sifu/Public/PlayerTest.h
@@ -19,11 +19,131 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
+	//virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	//hj 가 한출 추가
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//Spring Arm
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+		class USpringArmComponent* springArmComp;
+	//Camera Component
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+		class UCameraComponent* camComp;
+
+	////공격중 이동 중지
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool movementEnabled = true;
+	//이동속도
+	UPROPERTY(EditAnywhere, Category = PlayerSetting)
+		float walkSpeed = 600;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool isBlocking = false;
+	//이동방향
+	FVector direction;
+	//피격 몽타주
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* stomach;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* head2;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* head3;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* head4;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* falldown;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* death;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* block;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* punch;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* jab;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* uppercut;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* kick;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* highKick;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAnimMontage* lowKick;
+
+	//좌우 이동입력 이벤트 처리함수
+	void Turn(float value);
+	void LookUp(float value);
+	void InputHorizontal(float value);
+	void InputVertical(float value);
+	void InputJump();
+	void InputRun(bool run);
+	void AttackPunch();
+	void AttackKick();
+	UFUNCTION(BlueprintCallable)
+		void saveAttackCombo();
+	UFUNCTION(BlueprintCallable)
+		void punchCombo();
+	UFUNCTION(BlueprintCallable)
+		void kickCombo();
+	UFUNCTION(BlueprintCallable)
+		void ResetCombo();
+	UFUNCTION(BlueprintCallable)
+		void PlayerBlock(bool value);
+	UFUNCTION(BlueprintCallable)
+		void setTarget();
+	//공격받은 애니메이션 플레이 함수
+	UFUNCTION(BlueprintCallable)
+		void HurtAnim0();
+	UFUNCTION(BlueprintCallable)
+		void HurtAnim1();
+	UFUNCTION(BlueprintCallable)
+		void HurtAnim2();
+	UFUNCTION(BlueprintCallable)
+		void HurtAnim3();
+	UFUNCTION(BlueprintCallable)
+		void HurtAnim4();
+
+	//HJ 가 수정
+	UFUNCTION(BlueprintCallable)
+		void OnHitDamage();
+	UFUNCTION(BlueprintCallable)
+		void PlayerDamage();
+	UFUNCTION(BlueprintCallable)
+		void PlayerDie();
+
+	//타게팅 된 적
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class AHJ_Enemy* targetEnemy;
+	//타겟 여부 확인
+	UPROPERTY(EditAnywhere)
+		bool targetLock = false;
+
+	//체력
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		int32 hp = 5;
+	//죽음 확인
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool isDead = false;
+	//아래로 사라지는 속도
+	UPROPERTY(EditAnywhere)
+		float PlayerdieSpeed = 50.0f;
+
+	//공격 콤보
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int punchCount = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool IsAttacking = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool saveAttack = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int kickCount = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool kickorPunch = true;
 
 };
